fd94bdcf06309928e3d28a0f475a2e8b77bb2e5c
licf <756589738@qq.com>
2019-09-29 21:23:01 +0800

    创建ORBItSelf工程，这个主要是练手项目，使得orb算法不依赖第三方库

---
+++ testUtil/main.cpp
@@ -0,0 +1,23 @@
+/*!
+ * \file main.cpp
+ * \date 2019/09/17 22:23
+ *
+ * \author licf		
+ * Contact: licf.xdu@gmail.com
+ *
+ * \brief 
+ *
+ * TODO: long description
+ *
+ * \note
+*/
+
+#include <ORBextractor.h>
+
+using namespace cimg_library;
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
